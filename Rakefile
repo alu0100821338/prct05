0e045481aff18f4552b80b61a4aabbe6aef1def5
alu0100821338 <alu0100821338@ull.edu.es>
2015-10-25 20:31:24 +0000

    Probando test_suma con rake

--- Rakefile
+++ Rakefile
@@ -5,3 +5,8 @@ desc "Ejecutar solo las pruebas simples"
 task :test_simple do
   sh "ruby test/tc_racional.rb"
 end
+
+desc "Ejecutar solo las pruebas simples"
+task :test_suma do
+  sh "ruby test/tc_racional.rb"
+end
--- test/tc_racional.rb
+++ test/tc_racional.rb
@@ -5,8 +5,18 @@ class Test_Fraccion < Test::Unit::TestCase
 
   def test_simple
 	assert_equal("1/1", Fraccion.new(1, 1).to_s)
-	assert_equal("1/6", (Fraccion.new(1,2)*Fraccion.new(1,3)).to_s )
-    assert_equal("5/3", Fraccion.new(5, 3).to_s )
+	assert_equal("3/5", Fraccion.new(3, 5).to_s)
+  	assert_equal("2/3", Fraccion.new(4, 6).to_s)
+	assert_equal("-2/3", Fraccion.new(-4, 6).to_s)
   end
+
+  def test_suma
+	assert_equal("2/1", (Fraccion.new(1, 1)+Fraccion.new(1, 1)).to_s)
+	assert_equal("7/2", (Fraccion.new(3, 2)+Fraccion.new(4, 2)).to_s)
+	assert_equal("-1/2", (Fraccion.new(3, 2)+Fraccion.new(-4, 2)).to_s)
+	assert_equal("1/1", (Fraccion.new(6,8 )+Fraccion.new(2, 8)).to_s)
+	assert_equal("29/10", (Fraccion.new(7, 5)+Fraccion.new(3, 2)).to_s)
+  end
+
 
 end
